436cfbe0eb0dbaeffa9134a63b22439414c5d0aa
Hussein <hznagri@uwaterloo.ca>
2020-07-12 22:19:41 -0400

    controller to handle requests

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -1,6 +1,8 @@
 class CommunitiesController < ApplicationController
   before_action :authenticate_account!, except: [:index, :show]
+  before_action :set_community, only: [:show]
 
+  
   def index
     @communities = Community.all
   end
@@ -14,6 +16,22 @@ class CommunitiesController < ApplicationController
   end
 
   def create
+    @community = Community.new community_values
+    @comminity.account_id = current_account.id
 
+    if @comminity.save
+      redirect_to communities_path
+    else
+      render :new
+  end
+
+
+  private
+  def set_community
+    @community = Community.find(params[:id])
+  end
+  
+  def community_values 
+    params.require(:community).permit(:name, :url, :rules)
   end
 end
