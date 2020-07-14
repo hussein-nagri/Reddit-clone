b8e7e7ade1b886954f129182c703b02d2bc3934f
Hussein <hznagri@uwaterloo.ca>
2020-07-13 22:10:36 -0400

    community creation working

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -1,6 +1,8 @@
 class CommunitiesController < ApplicationController
   before_action :authenticate_account!, except: [:index, :show]
   before_action :set_community, only: [:show]
+  before_action :community_values, only: [:create]
+
 
   
   def index
@@ -17,14 +19,14 @@ class CommunitiesController < ApplicationController
 
   def create
     @community = Community.new community_values
-    @comminity.account_id = current_account.id
+    @community.account_id = current_account.id
 
-    if @comminity.save
+    if @community.save
       redirect_to communities_path
     else
       render :new
+    end
   end
-
 
   private
   def set_community
