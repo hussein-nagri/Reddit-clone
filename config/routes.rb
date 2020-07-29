e55203541d0f8a3c943dadb2e82db0d171b0075c
Hussein <hznagri@uwaterloo.ca>
2020-07-29 18:48:45 -0400

    setup post creation and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :accounts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #This file is for routes (resources generates all routes)
 
-  resources :communities
-
-
+  resources :communities do
+    resources :posts
+  end
   root to: "public#index"
 end
