8e650767c4aa371405ae136cf019129e541ccf8a
Hussein <hznagri@uwaterloo.ca>
2020-08-05 23:49:39 -0400

    show more info for posts

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #This file is for routes (resources generates all routes)
 
+  get "u/:username" => "profile#index", as: :profile
+
   resources :communities do
     resources :posts
   end
