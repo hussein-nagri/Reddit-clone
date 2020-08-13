b473288623f6369b2e3a7014f27cb186dc7ea31c
Hussein <hznagri@uwaterloo.ca>
2020-08-12 23:51:37 -0400

    working comments -> added js function to make in real time listing

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,6 +7,7 @@ class CommentsController < ApplicationController
     respond_to do |format|
       format.js {
         if @comment.save
+          @comments = Comment.where(post_id: @comment.post_id)
           render "comments/create"
         else 
             #unable to save
